dbac31faf0872114593902a6e005d3d65dc548fc
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-24 21:35:59 -0400

    feat(aliasing v3): improve performance of aliasing and re-rendering

--- src/related-queries/index.ts
+++ src/related-queries/index.ts
@@ -1,28 +1,30 @@
-import { alias, configurable, tag, utils, Events, Selectors, Store, Tag } from '@storefront/core';
+import { configurable, provide, tag, utils, Events, Selectors, Store, Tag } from '@storefront/core';
 
 @configurable
-@alias('relatedQueries')
+@provide('relatedQueries')
 @tag('gb-related-queries', require('./index.html'))
 class RelatedQueries {
-
   state: RelatedQueries.State = {
-    relatedQueries: []
+    relatedQueries: [],
   };
 
   init() {
-    this.updateRelatedQueries(this.select(Selectors.relatedQueries));
     this.subscribe(Events.RELATED_QUERIES_UPDATED, this.updateRelatedQueries);
   }
 
+  onBeforeMount() {
+    this.updateRelatedQueries(this.select(Selectors.relatedQueries));
+  }
+
   updateRelatedQueries = (relatedQueries: string[]) =>
-    !(relatedQueries.length === 0 && this.state.relatedQueries.length === 0)
-    && this.set({ relatedQueries: utils.mapToSearchActions(relatedQueries, <any>this.actions) })
+    !(relatedQueries.length === 0 && this.state.relatedQueries.length === 0) &&
+    this.set({ relatedQueries: utils.mapToSearchActions(relatedQueries, <any>this.actions) })
 }
 
-interface RelatedQueries extends Tag<any, RelatedQueries.State> { }
+interface RelatedQueries extends Tag<any, RelatedQueries.State> {}
 namespace RelatedQueries {
   export interface State {
-    relatedQueries: Array<{ value: string, onClick: () => void }>;
+    relatedQueries: Array<{ value: string; onClick: () => void }>;
   }
 }
 
